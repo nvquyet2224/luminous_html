41ca211bd439181e10165db15f4062c501cae9c5
nvquyet2224 <nvquyet2224@gmail.com>
2025-05-16 07:54:12 +0700

    update
    update

--- js/index1.js
+++ js/index1.js
@@ -1,13 +1,13 @@
 (function () {
         if (typeof jQuery !== "undefined" && typeof jQuery.fn.slick !== "undefined") {
             jQuery('.wb__believe--slider').slick({
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: true,
-            arrows: true,
-            autoplay: true,
-            autoplaySpeed: 3000,
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: true,
+                autoplay: true,
+                autoplaySpeed: 3000,
             });
             jQuery('.wb__university--slider > .vc_column-inner > .wpb_wrapper').slick({
             infinite: true,
@@ -15,7 +15,7 @@
             slidesToScroll: 1,
             dots: true,
             arrows: true,
-            autoplay: true,
+            //autoplay: true,
             autoplaySpeed: 3000,
             });
         } else {
