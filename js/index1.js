f7957233dcee30a3696acee663eed11416ba73f3
nvquyet2224 <nvquyet2224@gmail.com>
2025-06-15 17:30:38 +0700

    update
    test

--- js/index1.js
+++ js/index1.js
@@ -6,7 +6,7 @@
                 slidesToScroll: 1,
                 dots: true,
                 arrows: true,
-                autoplay: true,
+                //autoplay: true,
                 autoplaySpeed: 3000,
             });
             jQuery('.wb__university--slider > .vc_column-inner > .wpb_wrapper').slick({
